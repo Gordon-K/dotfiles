420f5b6e4cb6aca33af6feb2880074201bd2a416
Kernel Gordon <kernel.gordon@gmail.com>
2022-01-31 21:02:29 -0600

    rename theme in rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -50,7 +50,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 -- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/kernel/theme.lua")
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/kg_theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
