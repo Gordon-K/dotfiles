13e84b49639acd31658b212d66835cefef6ba20e
Kernel Gordon <kernel.gordon@gmail.com>
2022-02-04 22:18:19 -0600

    Update install script

--- install.sh
+++ install.sh
@@ -23,5 +23,24 @@ done
 echo "Updating packages with pacman"
 sudo pacman -S - < package_list.txt
 
+echo "Installing git repos"
+mkdir -p ~/Downloads
+# AwesomeWM Lain
+git clone https://github.com/lcpz/lain.git ~/.config/awesome/lain
+
+# Yay
+git clone https://aur.archlinux.org/yay.git ~/Downloads
+cd ~/Downloads/yay
+makepkg -si
+
+echo "Updating AUR packages with yay"
+# TODO: Make yay_packages file and loop to parse it like above for pacman
+yay -S ani-cli
+
+echo "Installing Rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rust.sh
+chmod +x rust.sh
+./rust.sh
+
 echo "Cleanup"
 rm package_list.txt
