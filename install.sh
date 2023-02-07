31c5f499daba041cb6eac4f1b883667f2f8c3685
Kernel Gordon <kernel.gordon@gmail.com>
2023-02-07 17:06:00 -0600

    new zsh and virtualization

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ echo "Updating config files"
 cp -r .config ~/
 echo "Overwriting xinitrc"
 cp .xinitrc ~/
+echo "Copying over Manjaro zsh config"
+cp -r zsh /usr/share/
+cp -r zsh-theme-powerlevel10k /usr/share/
 echo "Overwritting zshrc"
 cp .zshrc ~/
 
@@ -47,10 +50,6 @@ echo "Downloading Mononoki Nerdfont"
 mkdir -p $USERDIR/.local/share/fonts
 wget https://github.com/madmalik/mononoki/releases/download/1.3/mononoki.zip -O $USERDIR/Downloads/mononoki.zip
 unzip $USERDIR/Downloads/mononoki.zip -d $USERDIR/.local/share/fonts/mononoki
-echo "Downloading Creattion-Demo"
-mkdir -p $USERDIR/.local/share/fonts
-wget https://www.1001fonts.com/download/creattion-demo.zip -O $USERDIR/Downloads/creattion-demo.zip
-unzip $USERDIR/Downloads/creattion-demo.zip -d $USERDIR/.local/share/fonts/creattion-demo
 echo "Updating font cache"
 fc-cache
 
@@ -58,20 +57,4 @@ fc-cache
 rm -rf $USERDIR/.config/awesome/lain
 git clone https://github.com/lcpz/lain.git $USERDIR/.config/awesome/lain
 
-# Yay
-git clone https://aur.archlinux.org/yay.git $USERDIR/Downloads/yay
-cd $USERDIR/Downloads/yay
-makepkg -si
-
-echo "Updating AUR packages with yay"
-# TODO: Make yay_packages file and loop to parse it like above for pacman
-yay -S ani-cli --needed
-
-echo "Installing Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rust.sh
-chmod +x rust.sh
-./rust.sh
-
 echo "Cleanup"
-rm -rf $USERDIR/Downloads/yay
-rm $USERDIR/Downloads/rust.sh
