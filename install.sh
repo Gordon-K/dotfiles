1e97d09388e673bfbd811db464edc711dae5382c
Kernel Gordon <kernel.gordon@gmail.com>
2023-02-07 19:26:27 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,6 @@ echo "Updating config files"
 cp -r .config ~/
 echo "Overwriting xinitrc"
 cp .xinitrc ~/
-echo "Copying over Manjaro zsh config"
-cp -r zsh /usr/share/
-cp -r zsh-theme-powerlevel10k /usr/share/
-echo "Overwritting zshrc"
-cp .zshrc ~/
 
 IFS=$'\n'
 
@@ -31,6 +26,12 @@ do
   sudo pacman -S $package --needed --noconfirm
 done
 
+echo "Copying over Manjaro zsh config"
+cp -r zsh /usr/share/
+cp -r zsh-theme-powerlevel10k /usr/share/
+echo "Overwritting zshrc"
+cp .zshrc ~/
+
 echo "Change shell to ZSH"
 chsh -s $(which zsh)
 
@@ -44,6 +45,14 @@ curl -sL install-node.vercel.app/lts | sudo bash
 echo "Creating Downloads directory"
 mkdir -p $USERDIR/Downloads
 
+echo "Installing yay"
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+echo "Installing powerlevel10k"
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+
 echo "Installing git repos"
 # Mononoki Nerdfont
 echo "Downloading Mononoki Nerdfont"
@@ -51,10 +60,11 @@ mkdir -p $USERDIR/.local/share/fonts
 wget https://github.com/madmalik/mononoki/releases/download/1.3/mononoki.zip -O $USERDIR/Downloads/mononoki.zip
 unzip $USERDIR/Downloads/mononoki.zip -d $USERDIR/.local/share/fonts/mononoki
 echo "Updating font cache"
-fc-cache
+fc-cache -vf
 
 # AwesomeWM Lain
 rm -rf $USERDIR/.config/awesome/lain
 git clone https://github.com/lcpz/lain.git $USERDIR/.config/awesome/lain
 
 echo "Cleanup"
+rm -rf $USERDIR/Downloads/yay
